a7d576bcae273f450c854f9fd5d174bfc4cb7aeb
Matthew O'Boyle <oboyle.matthew@duke.edu>
2020-01-26 22:53:21 -0500

    Starting world view

---
+++ src/figma/Connect/Connect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Connect.css';
+import ibby from '../../images/ibby_cutout.png';
+
+function Connect() {
+    return (
+        <div className={"connect-container"}>
+            <div id={'connect-text'}>Connect With Ibby</div>
+        </div>
+    );
+}
+
+export default Connect;
