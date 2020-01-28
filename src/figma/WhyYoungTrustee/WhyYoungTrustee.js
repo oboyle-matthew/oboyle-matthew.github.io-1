4ab39afcaa69ed0e568b61e0354745b69628728f
Feroze Mohideen <fm87@duke.edu>
2020-01-28 12:41:00 -0500

    styling whyyoungtrusteesection

--- src/figma/WhyYoungTrustee/WhyYoungTrustee.js
+++ src/figma/WhyYoungTrustee/WhyYoungTrustee.js
@@ -4,21 +4,39 @@ import './WhyYoungTrustee.css';
 function WhyYoungTrustee() {
     return (
         <div id={"why-container"}>
-            <div id={"left-background"}/>
-            <div className="value-title" id={"community-title"}>COMMUNITY</div>
-            <div className="value-description" id={"community-description"}>
-                Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+            <div id={"left-background"} />
+            <div class="three-container">
+                <div class="descriptionitem">
+                    <div className="value-title" id={"community-title"}>COMMUNITY</div>
+                    <div class="row">
+                        <div className="value-description" id={"description"}>
+                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                        </div>
+                    </div>
+                </div>
+                <div class="descriptionitem">
+                    <div className="value-title" id={"opportunity-title"}>OPPORTUNITY</div>
+                    <div class="row">
+                        <div className="value-description" id={"description"}>
+                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                    </div>
+                    </div>
+                </div>
+                <div class="descriptionitem">
+                    <div className="value-title" id={"experience-title"}>EXPERIENCE</div>
+                    <div class="row">
+                        <div className="value-description" id={"description"}>
+                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                    </div>
+                    </div>
+                </div>
+
+
+
+
+
             </div>
 
-            <div className="value-title" id={"opportunity-title"}>OPPORTUNITY</div>
-            <div className="value-description" id={"opportunity-description"}>
-                Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
-            </div>
-
-            <div className="value-title" id={"experience-title"}>EXPERIENCE</div>
-            <div className="value-description" id={"experience-description"}>
-                Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
-            </div>
 
 
 
