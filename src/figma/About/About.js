8a4a5dfef6a29c067c486fe62fdd69e42f6f4532
Feroze Mohideen <fm87@duke.edu>
2020-01-29 10:46:09 -0500

    finished about and whyYT

--- src/figma/About/About.js
+++ src/figma/About/About.js
@@ -13,11 +13,22 @@ function About() {
                 <img src={ibby} id={'ibby-cutout'} />
             </div>
             <div class="beige-area">
-                <div id={"about-ibby"}>About ibby</div>
+                <div id={"about-ibby"}>About Ibby</div>
 
                 <div class="row">
                 <div id={"text-container"}>
-                Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool.
+                <p>Hello my fellow Dukies! </p>
+
+<p>I want to begin by saying that my journey to Duke was unconventional. Duke was not made for people like me. However, even after Duke had thrown so many barriers in my way, from a poor financial aid package, to engaging in Islamophobic sentiments I have persisted and tried to ensure that in every action I take, I am making this university a better place for students now and in the future.</p> 
+
+<p>From serving as the co-president of Duke Low Income-First Generation students I have helped build a thriving community of hundreds of students that now have a dedicated campus space. I have mentored low-income students in Durham through the Duke-Durham School Days Initiative, and I have been an advocate for the Community Empowerment Fund to help Durham residents strive for economic justice.</p>
+
+<p>I’m the only candidate to sit on a Board of Trustees Committee - The Undergraduate Education Committee - where I actively lobby the Trustees to consider how their decisions impact students on the ground. </p>
+
+<p>As Young Trustee I guarantee to you that I will never waiver in my dedication to ensuring that in every conversation we have, we consider the voices of those not present at the table and I will make it my daily mission to leave this university in a better state than when I first came here. </p>
+ 
+<p>Join me in creating a more equitable and inclusive Duke, from the ground up! </p>
+
     </div> 
                 </div>
                 <div class="row2">
--- src/figma/WhyYoungTrustee/WhyYoungTrustee.js
+++ src/figma/WhyYoungTrustee/WhyYoungTrustee.js
@@ -13,7 +13,7 @@ function WhyYoungTrustee() {
                     <div className="value-title" id={"community-title"}>COMMUNITY</div>
                     <div class="row">
                         <div className="value-description" id={"description"}>
-                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                        My first and foremost priority at Duke is the continuous investment in building diverse and welcoming communities. Every step of the way I have advocated for the Duke and Durham community from being on the Centre for Race Relations Executive Board to assisting with the Duke-Durham School Days Initiative. 
                         </div>
                     </div>
                 </div>
@@ -21,7 +21,7 @@ function WhyYoungTrustee() {
                     <div className="value-title" id={"opportunity-title"}>OPPORTUNITY</div>
                     <div class="row">
                         <div className="value-description" id={"description"}>
-                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                        Ensuring that every student has equal opportunities to access all of Duke’s resources regardless of their background is one of the core tenets of my work as the co-President of Duke LIFE. Serving as a You @ Duke facilitator during Orientation Week was also because I believe every Duke student, from the first day they step on this campus should be considering how they can improve the experience of every member of our Duke family.
                     </div>
                     </div>
                 </div>
@@ -29,7 +29,7 @@ function WhyYoungTrustee() {
                     <div className="value-title" id={"experience-title"}>EXPERIENCE</div>
                     <div class="row">
                         <div className="value-description" id={"description"}>
-                            Ibby is cool. Very cool. Very very very very very very very very very very very very very very very very very very very very very cool. Ibby is cool.
+                        Being able to sit on the Undergraduate Education Committee of the Board of Trustees this past year has been one of the greatest privileges of my time at Duke. I have learned how to confidently voice my understanding of the student experience and advocate for increased awareness of students' needs at the highest level of decision making at our university.
                     </div>
                     </div>
                 </div>
