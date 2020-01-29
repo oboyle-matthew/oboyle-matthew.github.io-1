a5c8059bbdf25d58b5e5a1bb00d0667ba11a195b
Feroze Mohideen <fm87@duke.edu>
2020-01-29 10:22:05 -0500

    finished quotes section

--- src/figma/Friends/Friends.js
+++ src/figma/Friends/Friends.js
@@ -3,6 +3,12 @@ import './Friends.css';
 import ReactSwipe from 'react-swipe';
 import buddies from '../../images/BestBuddies.jpg';
 import leftarrow from '../../images/left-arrow.svg';
+import antepli from '../../images/quotes/Abdullah Antepli.jpg';
+import feroze from '../../images/quotes/feroze.jpg';
+import erin from '../../images/quotes/erin.jpg';
+import jenny from '../../images/quotes/jenny.jpg';
+import brooke from '../../images/quotes/brooke.jpg';
+
 
 
 function Friends() {
@@ -10,32 +16,32 @@ function Friends() {
         {
             author: 'Erin McDermott',
             year: '2020',
-            image: buddies,
+            image: erin,
             quote: 'He cares about pushing what makes Duke a community, what makes Duke a home, pushing it forward. He’s the kind of person that, whenever anyone would need him to be there or would need help, he will drop everything and be that person.'
         },
         {
             author: 'Jenny Chin',
             year: '2020',
-            image: buddies,
+            image: jenny,
             quote: 'Ibby really steps up and commits himself for the people he cares about, and luckily for the student body that includes all of us.'
         },
         {
-            author: 'Abdullah Antepli (via the Duke Chronicle)',
+            author: 'Prof. Abdullah Antepli (via the Duke Chronicle)',
             year: '2020',
-            image: buddies,
+            image: antepli,
             quote: 'He has this burning fire in him. He understands the world through the lens of social justice ... that’s the goodness of this man who is keenly aware of his privileges, and he is so committed to turn his privileges into some sort of service, some sort of commitment.'
         },
         {
             author: 'Brooke Porter',
             year: '2020',
-            image: buddies,
+            image: brooke,
             quote: 'Since I met him our freshman year, Ibby has been dedicated to communities at Duke and our partners in Durham. His inner drive and work ethic never fail to impress me.'
         },
         {
             author: 'Feroze Mohideen',
             year: '2020',
-            image: buddies,
-            quote: 'There\`s no doubt Ibby is involved in a lot of things at Duke or within Durham (or so I\'ve been told), but to me, Ibby is plainly not only a true friend but someone I\'d call my brother. Whether it be obnoxious 3 am Taylor Swift karaoke or guilty late-night Krafthouse runs, Ibby has been there for me through thick and thin, and I know he\'ll carry the same energy into every relationship he makes.'
+            image: feroze,
+            quote: 'There\`s no doubt Ibby is involved in a lot of things at Duke or within Durham, but to me, Ibby is plainly not only a true friend but someone I\'d call my brother. Whether it be 3 am Taylor Swift karaoke or guilty late-night Krafthouse runs, Ibby has been there for me through thick and thin, and I know he\'ll carry the same energy into every relationship he makes.'
         },
     ];
     let reactSwipeEl;
